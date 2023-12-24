ac1654ba06116572bf4648b2c84effa1cc00be02
Roy Ivy III <rivy.dev@gmail.com>
2023-12-24 02:02:08 -0600

    wip ~ wip better name parsing

--- src/utils/parseAuthor.spec.ts
+++ src/utils/parseAuthor.spec.ts
@@ -2,8 +2,8 @@ import { parseAuthors } from './parseAuthor';
 
 describe('parseAuthor', () => {
   it.each([
-    ['Michael Port', 'Michael', 'Port'],
-    ['Strunk Jr., William', 'William', 'Strunk Jr'],
+    ['Michael Port M.D.', 'Michael', 'Port'],
+    ['Strunk Jr., William', 'William', 'Strunk'],
     ['Yuval Noah Harari', 'Yuval', 'Harari'],
     ['P2K', undefined, 'P2K'],
     ['Toole, John Kennedy', 'John', 'Toole'],
@@ -11,12 +11,10 @@ describe('parseAuthor', () => {
     ['Schwab, V. E.', 'V', 'Schwab'],
     ['Brandon Sanderson', 'Brandon', 'Sanderson'],
     ['Sanderson, Brandon', 'Brandon', 'Sanderson'],
-  ])(
-    'Parse "%s" evaluated as firstName: "%s" and lastName: "%s',
-    (author, firstName, lastName) => {
-      const authors = parseAuthors(author);
-      expect(authors).toHaveLength(1);
-      expect(authors[0]).toEqual({ firstName, lastName });
+  ])('"%s" is parsed as firstName: "%s" and lastName: "%s"', (author, firstName, lastName) => {
+    const authors = parseAuthors(author);
+    expect(authors).toHaveLength(1);
+    expect(authors[0]).toEqual({ firstName, lastName });
     }
   );
 
@@ -26,7 +24,7 @@ describe('parseAuthor', () => {
     ['Robert Kegan aNd Lisa Laskow Lahey', 2],
     ['Chan, Francis;Sprinkle, Preston', 2],
   ])('"%s" is parsed as %s author(s)', (author, expectedAuthorCount) => {
-    const authors = parseAuthors(author);
-    expect(authors).toHaveLength(expectedAuthorCount);
+      const authors = parseAuthors(author);
+      expect(authors).toHaveLength(expectedAuthorCount);
   });
 });
--- src/utils/parseAuthor.ts
+++ src/utils/parseAuthor.ts
@@ -1,7 +1,4 @@
-type Author = {
-  firstName: string;
-  lastName: string;
-};
+type Author = { firstName: string; lastName: string };
 
 export const parseAuthors = (author: string | undefined): Author[] => {
   if (author == null) {
@@ -17,20 +14,44 @@ export const parseAuthors = (author: string | undefined): Author[] => {
   }
 
   if (author.includes(';')) {
-    return author
-      .split(';')
-      .map((a) => a.trim())
-      .map(parseSingleAuthor);
+    return author.split(';').map((a) => a.trim()).map(parseSingleAuthor);
   }
 
   return [parseSingleAuthor(author)];
 };
 
+const commonGenerationalSuffixes = ['I', 'II', 'III', 'IV', 'V', 'Jr[.]?', 'Sr[.]?'];
+const commonHonorificPrefixes = ['Dr[.]?', 'Miss', 'Mr[.]?', 'Mrs[.]?', 'Ms[.]?'];
+const commonHonorificSuffixes = ['M[.]?D[.]?', 'Ph[.]?D[.]?'];
+
 const parseSingleAuthor = (author: string): Author => {
-  const hasComma = author.includes(',');
+  // parse a "western-tradition" name (or names) with possible honorifics and suffixes
+  // ref: https://stackoverflow.com/questions/17455658/human-name-parsing
+  // ref: https://www.humangraphics.io/blog/whats-in-a-name-part-1-introduction
+  // note: due to the complexity and ambiguity in parsing names, this is a best-effort attempt
+
+  const modifiedAuthor = author
+    // * normalize any comma-separated suffixes
+    .replace(
+      new RegExp(
+        `(?:,\s*)?\b(${
+          [...commonGenerationalSuffixes, ...commonHonorificSuffixes].join('|')
+        })(?:\b|$)`,
+        'gim',
+      ),
+      (_match, substr) => (substr as string).replace('.', ''),
+    )
+    // * remove generational suffixes
+    .replace(new RegExp(`\b(${commonGenerationalSuffixes.join('|')})\b`, 'gim'), '')
+    // * remove honorific prefixes
+    .replace(new RegExp(`\b(${commonHonorificPrefixes.join('|')})\b`, 'gim'), '')
+    // * remove honorific suffixes
+    .replace(new RegExp(`\b(${commonHonorificSuffixes.join('|')})\b`, 'gim'), '');
+
+  const hasComma = modifiedAuthor.includes(',');
 
   if (hasComma) {
-    const names = splitAndTrim(',', author);
+    const names = splitAndTrim(',', modifiedAuthor);
     return {
       firstName: names.length == 1 ? undefined : splitAndTrim(' ', names[names.length - 1])[0],
       lastName: names[0],
@@ -38,15 +59,13 @@ const parseSingleAuthor = (author: string): Author => {
   }
 
   const names = splitAndTrim(' ', author);
-  return {
-    firstName: names.length == 1 ? undefined : names[0],
-    lastName: names[names.length - 1],
-  };
+  return { firstName: names.length == 1 ? undefined : names[0], lastName: names[names.length - 1] };
 };
 
 const splitAndTrim = (needle: string, author: string): string[] => {
   return author
     .split(needle)
     .map((a) => a.trim())
-    .map((a) => a.replace(/\.$/, ''));
+    .map((a) => a.replace(/\.$/, ''))
+    .filter((a) => a !== '');
 };
